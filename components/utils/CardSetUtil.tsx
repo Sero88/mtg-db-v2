8bc15c4987349d5f6200ee9f2dcb7607d5608e6a
Sergio <seres.crt@gmail.com>
2024-06-15 08:08:29 -0500

    feat: adds icon and styles to set item

--- components/utils/CardSetUtil.tsx
+++ components/utils/CardSetUtil.tsx
@@ -1,6 +1,8 @@
 import { ScryfallSet } from "@/types/scryfall";
 import { SelectorListItem } from "@/types/searchSelector";
 import { SetUtil } from "@/utils/setUtil";
+import Image from "next/image";
+import styles from "@/styles/cardSets.module.scss";
 
 export function createSetsArray(scryfallSets: ScryfallSet[], collectionSets: string[]) {
 	const searchSetSelectorList = [] as SelectorListItem[];
@@ -14,6 +16,19 @@ export function createSetsArray(scryfallSets: ScryfallSet[], collectionSets: str
 		}
 
 		collectionSets.forEach((set: string) => {
+			const displaySet = (
+				<div className={styles.setDisplay}>
+					<Image
+						src={scryfallSet.icon_svg_uri}
+						width={15}
+						height={15}
+						unoptimized={true}
+						alt={scryfallSet.name}
+						key={`${scryfallSet.name}-${scryfallSet.code}`}
+					/>
+					<p className={styles.setName}>{scryfallSet.name}</p>
+				</div>
+			);
 			// scryfall broke the 3 letter mtg set convetion, so some sets on scryfall have more than 3 letters such as hrt20
 			// parent_set_code is set for scryfall sets such as promo and tokens, the parent is the set we want, so ignore the child
 			// lastly, if none of the above worked, convert the set into three letters and check againts it
@@ -22,7 +37,7 @@ export function createSetsArray(scryfallSets: ScryfallSet[], collectionSets: str
 				(!scryfallSet.parent_set_code && SetUtil.getCardSet(scryfallSet.code) == set)
 			) {
 				searchSetSelectorList.push({
-					display: <p>{scryfallSet.name}</p>,
+					display: displaySet,
 					value: set,
 					searchValue: scryfallSet.name,
 				});
