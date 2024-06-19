015f309d131d860ed106f9355eeb9a316e072fb7
Sergio <seres.crt@gmail.com>
2024-06-19 16:18:39 -0500

    feat: CardRarity check/uncheck functionality

--- components/search/fields/CardRarity.spec.tsx
+++ components/search/fields/CardRarity.spec.tsx
@@ -1,6 +1,6 @@
 import { SearchFieldNames } from "@/types/search";
 import { CardRarity } from "./CardRarity";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CardRarityEnum } from "@/types/card";
 const fieldData = {
 	name: SearchFieldNames.RARITY,
@@ -19,5 +19,26 @@ describe("CardRarity", () => {
 
 		const checkboxes = screen.queryAllByRole("checkbox");
 		expect(checkboxes.length).toEqual(Object.keys(CardRarityEnum).length / 2);
+	});
+
+	it("should run changeHandler when checkbox is clicked", () => {
+		renderComponent();
+
+		const checkboxes = screen.queryAllByRole("checkbox");
+		fireEvent.click(checkboxes[0]);
+
+		expect(changeHandler).toHaveBeenCalledWith([0]);
+	});
+
+	it("should remove item when unchecked", () => {
+		renderComponent();
+
+		const checkboxes = screen.queryAllByRole("checkbox");
+		fireEvent.click(checkboxes[0]);
+
+		expect(changeHandler).toHaveBeenCalledWith([0]);
+
+		fireEvent.click(checkboxes[0]);
+		expect(changeHandler).toHaveBeenLastCalledWith([]);
 	});
 });
--- components/search/fields/CardRarity.tsx
+++ components/search/fields/CardRarity.tsx
@@ -5,13 +5,14 @@ import { useState } from "react";
 type CardRarityProps = {
 	fieldData: {
 		name: SearchFieldNames;
-		value: string[];
+		value: number[];
 	};
-	changeHandler: (value: any) => void;
+	changeHandler: (newSelection: number[]) => void;
 };
+
 export function CardRarity({ changeHandler, fieldData }: CardRarityProps) {
 	const rarities = [];
-	const [selectedRarities, setSelectedSRarities] = useState(new Set());
+	const [selectedRarities, setSelectedSRarities] = useState(new Set<number>());
 
 	const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>, rarity: CardRarityEnum) => {
 		const newSetSelection = new Set(selectedRarities);
@@ -21,6 +22,7 @@ export function CardRarity({ changeHandler, fieldData }: CardRarityProps) {
 			newSetSelection.delete(rarity);
 		}
 		setSelectedSRarities(newSetSelection);
+		changeHandler(Array.from(newSetSelection));
 	};
 
 	for (const rarityEnum in CardRarityEnum) {
@@ -33,28 +35,27 @@ export function CardRarity({ changeHandler, fieldData }: CardRarityProps) {
 
 		//create the checkboxes
 		rarities.push(
-			<label key={rarity}>
-				<input
-					type="checkbox"
-					data-value={rarity}
-					checked={selectedRarities.has(rarity)}
-					onChange={(e) => onChangeHandler(e, rarity)}
-				/>
-				<span>
-					{rarity == CardRarityEnum.specialBonus
-						? "Special/Bonus"
-						: CardRarityEnum[rarity]}
-				</span>
-			</label>
+			<li key={rarity}>
+				<label>
+					<input
+						type="checkbox"
+						checked={selectedRarities.has(rarity)}
+						onChange={(e) => onChangeHandler(e, rarity)}
+					/>
+					<span>
+						{rarity == CardRarityEnum.specialBonus
+							? "Special/Bonus"
+							: CardRarityEnum[rarity]}
+					</span>
+				</label>
+			</li>
 		);
 	}
 
 	return (
 		<>
 			<h2>Card Rarity</h2>
-			<div>
-				<div>{rarities}</div>
-			</div>
+			<ul>{rarities}</ul>
 		</>
 	);
 }
