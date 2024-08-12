8c3dc78b4930bb37ef739c2bcefedc220cdca80d
Sergio <seres.crt@gmail.com>
2024-08-12 16:25:36 -0500

    feat: adds reports refresh handler

--- app/collection/reports/page.tsx
+++ app/collection/reports/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useContext, useMemo } from "react";
+import { useContext, useMemo, useState } from "react";
 
 import { CollectionVersionsContext } from "@/contexts/CollectionVersionsContext";
 import { ReportData } from "@/types/reports";
@@ -7,65 +7,83 @@ import { getReportData } from "@/utils/reportsUtil";
 import styles from "@/styles/reports.module.scss";
 import { QuantityRarityTable } from "@/components/reports/QuantityRarityTable";
 import { UpdatePrices } from "@/components/reports/UpdatePrices";
+import { useGetCollectionVersions } from "@/hooks/useGetCollectionWithVersions";
+import { QueryResult } from "@/components/utils/QueryResult";
 
 export default function SearchPage() {
-	const versions = useContext(CollectionVersionsContext);
+	const [lastUpdate, setLastUpdate] = useState<Date>();
 
-	const reportData: ReportData = useMemo(() => getReportData(versions), [versions]);
+	const versions = useContext(CollectionVersionsContext);
+	const getVersionsResult = useGetCollectionVersions(lastUpdate);
+
+	const versionsData = getVersionsResult.data || versions;
+
+	const reportData: ReportData = useMemo(() => getReportData(versionsData), [versionsData]);
 
 	return (
 		<>
-			<div className={styles.reports}>
-				<h1>Collection Data Overview:</h1>
+			<QueryResult
+				queryResult={{
+					error: !!getVersionsResult.error,
+					data: versions || getVersionsResult.data,
+					isLoading: getVersionsResult.isLoading,
+				}}
+				errorMessage="Unable to retrieve collection data. Please try refreshing the page."
+			>
+				<div className={styles.reports} data-testid="reports-section">
+					<h1>Collection Data Overview:</h1>
 
-				<div className={styles.reportSection}>
-					<h2>Quantity Data:</h2>
-					<ul>
-						<li>
-							<span>Regular Cards:</span> {reportData.quantity.regular}
-						</li>
-						<li>
-							<span>Foil Cards:</span> {reportData.quantity.foil}
-						</li>
-						<li>
-							<span>Unique Cards:</span> {reportData.quantity.unique}
-						</li>
-						<li>
-							<span>By Rarities:</span>{" "}
-							<QuantityRarityTable reportData={reportData} />
-						</li>
+					<div className={styles.reportSection}>
+						<h2>Quantity Data:</h2>
+						<ul>
+							<li>
+								<span>Regular Cards:</span> {reportData.quantity.regular}
+							</li>
+							<li>
+								<span>Foil Cards:</span> {reportData.quantity.foil}
+							</li>
+							<li>
+								<span>Unique Cards:</span> {reportData.quantity.unique}
+							</li>
+							<li>
+								<span>By Rarities:</span>{" "}
+								<QuantityRarityTable reportData={reportData} />
+							</li>
 
-						<li>
-							<span>Total Cards:</span> {reportData.quantity.total}
-						</li>
-						<li>
-							<span>Sets:</span> {reportData.quantity.sets}
-						</li>
-					</ul>
+							<li>
+								<span>Total Cards:</span> {reportData.quantity.total}
+							</li>
+							<li>
+								<span>Sets:</span> {reportData.quantity.sets}
+							</li>
+						</ul>
+					</div>
+
+					<div className={styles.reportSection}>
+						<h2>Price Data:</h2>
+						<ul>
+							<li>
+								<span>Regular Cards:</span> ${reportData.prices.regular.toFixed(2)}
+							</li>
+							<li>
+								<span>Foil Cards:</span> ${reportData.prices.foil.toFixed(2)}
+							</li>
+							<li>
+								<span>Rares and Mythics only:</span> $
+								{reportData.prices.rares.toFixed(2)}
+							</li>
+							<li>
+								<span>Total Cards:</span> ${reportData.prices.total.toFixed(2)}
+							</li>
+						</ul>
+					</div>
 				</div>
-
-				<div className={styles.reportSection}>
-					<h2>Price Data:</h2>
-					<ul>
-						<li>
-							<span>Regular Cards:</span> ${reportData.prices.regular.toFixed(2)}
-						</li>
-						<li>
-							<span>Foil Cards:</span> ${reportData.prices.foil.toFixed(2)}
-						</li>
-						<li>
-							<span>Rares and Mythics only:</span> $
-							{reportData.prices.rares.toFixed(2)}
-						</li>
-						<li>
-							<span>Total Cards:</span> ${reportData.prices.total.toFixed(2)}
-						</li>
-					</ul>
-				</div>
-			</div>
+			</QueryResult>
 
 			<div className={styles.reportSection}>
-				<UpdatePrices updateCompleteCallback={() => console.log("updating time")} />
+				<UpdatePrices
+					updateCompleteCallback={(newUpdateDate) => setLastUpdate(newUpdateDate)}
+				/>
 			</div>
 		</>
 	);
--- app/collection/reports/reports.spec.tsx
+++ app/collection/reports/reports.spec.tsx
@@ -1,6 +1,9 @@
 import ReportsPage from "./page";
-import { screen, render } from "@testing-library/react";
+import { screen, render, queryByTestId } from "@testing-library/react";
 import * as UpdatePricesComponent from "@/components/reports/UpdatePrices";
+import { useGetCollectionVersions } from "@/hooks/useGetCollectionWithVersions";
+import { UseQueryResult } from "react-query";
+import * as QueryResultComponent from "@/components/utils/QueryResult";
 
 jest.mock("@/components/reports/UpdatePrices", () => {
 	const originalModule = jest.requireActual("@/components/reports/UpdatePrices");
@@ -10,9 +13,35 @@ jest.mock("@/components/reports/UpdatePrices", () => {
 	};
 });
 
+jest.mock("@/components/utils/QueryResult", () => {
+	const originalModule = jest.requireActual("@/components/utils/QueryResult");
+	return {
+		__esModule: true,
+		...originalModule,
+	};
+});
+
 const updatePricesSpy = jest.spyOn(UpdatePricesComponent, "UpdatePrices");
+const queryResultSpy = jest.spyOn(QueryResultComponent, "QueryResult");
+
+jest.mock("@/hooks/useGetCollectionWithVersions");
+const useGetCollectionVersionsMock = jest.mocked(useGetCollectionVersions);
 
 describe("/collection/search page", () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+		useGetCollectionVersionsMock.mockReturnValue({
+			isLoading: false,
+			error: false,
+			data: undefined,
+			isSuccess: false,
+		} as unknown as UseQueryResult);
+	});
+
+	it("should display reports section on load", () => {
+		render(<ReportsPage />);
+		expect(screen.queryByTestId("reports-section")).not.toBeNull();
+	});
 	it("should display header", () => {
 		render(<ReportsPage />);
 		expect(screen.queryByRole("heading", { level: 1 })).not.toBeNull();
@@ -31,5 +60,34 @@ describe("/collection/search page", () => {
 	it("should display UpdatePrices section", () => {
 		render(<ReportsPage />);
 		expect(updatePricesSpy).toHaveBeenCalled();
+	});
+
+	it("should show loader when fetching version data", () => {
+		useGetCollectionVersionsMock.mockReturnValue({
+			isLoading: true,
+			error: false,
+			data: undefined,
+			isSuccess: false,
+		} as unknown as UseQueryResult);
+
+		render(<ReportsPage />);
+		expect(screen.queryByTestId("loader")).not.toBeNull();
+	});
+
+	it("should use QueryResult", () => {
+		render(<ReportsPage />);
+		expect(queryResultSpy).toHaveBeenCalled();
+	});
+
+	it("should not display reports section when loading versions", () => {
+		useGetCollectionVersionsMock.mockReturnValue({
+			isLoading: true,
+			error: false,
+			data: undefined,
+			isSuccess: false,
+		} as unknown as UseQueryResult);
+		render(<ReportsPage />);
+		expect(screen.queryByTestId("reports-section")).toBeNull();
+		expect(screen.queryByTestId("loader")).not.toBeNull();
 	});
 });
