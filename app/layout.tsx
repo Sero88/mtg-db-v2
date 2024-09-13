2c7087b3b6db5d38c20af57f3d0aca9afbf74699
Sergio <seres.crt@gmail.com>
2024-09-13 16:16:07 -0500

    feat: adds header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Saira } from "next/font/google";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/utils/authOptions";
 import { Providers } from "./providers";
+import { Header } from "@/components/template/Header";
 
 const saira = Saira({ weight: ["200", "400"], subsets: ["latin"] });
 
@@ -18,8 +19,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 		<html lang="en">
 			<body className={saira.className}>
 				<Providers session={session}>
-					<main>
-						<div className="wrapper">{children}</div>
+					<main className="wrapper">
+						<Header />
+						<div>{children}</div>
 					</main>
 				</Providers>
 			</body>
