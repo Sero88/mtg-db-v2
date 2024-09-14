26480c72fe1c22ca98b44d3e629554341c50b6f2
Sergio <seres.crt@gmail.com>
2024-09-14 08:47:07 -0500

    feat: adds collection options to homepage

--- app/homepage.spec.tsx
+++ app/homepage.spec.tsx
@@ -1,15 +1,27 @@
 import { DailyFlavorText } from "@/components/collection/DailyFlavorText";
 import Homepage from "./page";
 import { render } from "@testing-library/react";
+import { CollectionOption } from "@/components/utils/CollectionOption";
 
 jest.mock("@/components/collection/DailyFlavorText");
 const dailyFlavorTextMock = jest.mocked(DailyFlavorText);
 
+jest.mock("@/components/utils/CollectionOption");
+const collectionOptionMock = jest.mocked(CollectionOption);
+
 dailyFlavorTextMock.mockReturnValue(<></>);
 
 describe("Homepage", () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
 	it("should display DailyFlavorText", () => {
 		render(<Homepage />);
 		expect(dailyFlavorTextMock).toHaveBeenCalled();
+	});
+
+	it("should display collection options", () => {
+		render(<Homepage />);
+		expect(collectionOptionMock).toHaveBeenCalledTimes(2);
 	});
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
 import { DailyFlavorText } from "@/components/collection/DailyFlavorText";
+import { CollectionOption } from "@/components/utils/CollectionOption";
 
 export default function Home() {
-	return <DailyFlavorText />;
+	return (
+		<>
+			<DailyFlavorText />
+			<div>
+				<CollectionOption
+					mainImage="/images/cards-add-image.png"
+					imageBg="/images/add-polygon.png"
+					title="Search Collection"
+					mainImagePosition="right"
+					link="/collection/search"
+				/>
+
+				<CollectionOption
+					mainImage="/images/cards-search-image.png"
+					imageBg="/images/search-polygon.png"
+					title="Add to Collection"
+					mainImagePosition="left"
+					link="/collection/add"
+				/>
+			</div>
+		</>
+	);
 }
