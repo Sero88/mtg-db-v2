52fe815167f43a2b0cab9c373d4e0e81312b65e4
Sergio <seres.crt@gmail.com>
2024-09-17 16:42:35 -0500

    feat: adds server session test

--- app/api/collection/flavor-text/route.ts
+++ app/api/collection/flavor-text/route.ts
@@ -2,8 +2,16 @@ import { Helpers } from "@/utils/helpers";
 import { NextResponse } from "next/server";
 import { CardCollection } from "@/models/cardCollection";
 import { DbModelResponseEnum } from "@/types/utils";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/utils/authOptions";
 
 export async function GET() {
+	const session = await getServerSession(authOptions);
+
+	//todo remove after testing 👇
+	console.log("session ===> ", session);
+	//todo remove after testing 👆
+
 	const cardCollection = new CardCollection();
 	const isConnected = await cardCollection.dbConnect();
 
