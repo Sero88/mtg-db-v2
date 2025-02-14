d8633a715f15eba69857b0ba66c0ba3807f03d2f
Sergio <seres.crt@gmail.com>
2025-02-13 21:48:37 -0600

    test: using latest version for deployment

--- components/cards/CardList.tsx
+++ components/cards/CardList.tsx
@@ -1,10 +1,11 @@
 import styles from "@/styles/results.module.scss";
 import { CollectionCard as CollectionCardType } from "@/types/collection";
 import { ScryfallCard } from "@/types/scryfall";
+import { ReactElement } from "react";
 
 type CardListProps = {
 	cardData: CollectionCardType[] | ScryfallCard[];
-	renderCard: (card: CollectionCardType | ScryfallCard) => JSX.Element;
+	renderCard: (card: CollectionCardType | ScryfallCard) => ReactElement;
 };
 export function CardList({ cardData, renderCard }: CardListProps) {
 	return (
